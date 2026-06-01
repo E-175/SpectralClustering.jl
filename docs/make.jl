43162cddda68c80357e306a5b9db16ecbb1ef5a3
janus_tro <trotzer@campus.tu-berlin.de>
2026-06-01 12:48:39 +0200

    Add group members to authors and update .gitignore

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ DocMeta.setdocmeta!(SpectralClustering, :DocTestSetup, :(using SpectralClusterin
 
 makedocs(;
     modules=[SpectralClustering],
-    authors="Jens Eckert <j.eckert.1@campus.tu-berlin.de>",
+    authors="Jens Eckert <j.eckert.1@campus.tu-berlin.de>, Christoph Seidelmann <c.seidelmann@campus.tu-berlin.de>, Carolin Witschonke <c.witschonke@campus.tu-berlin.de>, Janus Trotzer <trotzer@campus.tu-berlin.de>",
     sitename="SpectralClustering.jl",
     format=Documenter.HTML(;
         canonical="https://E-175.github.io/SpectralClustering",
