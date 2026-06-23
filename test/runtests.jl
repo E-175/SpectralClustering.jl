5c9bfbb8494b510c7c0d68c492635cda180cbce6
E-175 <j.eckert.1@campus.tu-berlin.de>
2026-06-23 21:04:58 +0200

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,11 +6,20 @@ using Test
     include("test_affinity.jl")
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     include("test_self_tuning.jl")
 =======
+=======
+>>>>>>> 81a6ea7 (tests)
     include("test_discretization.jl")
 >>>>>>> beab6e2 (feat: SVD Discretization implemented)
 =======
     include("test_laplacian.jl")
+<<<<<<< HEAD
 >>>>>>> 2e4bfa5 (Add some tests)
+=======
+=======
+    include("test_self_tuning.jl")
+>>>>>>> f84002b (tests)
+>>>>>>> 81a6ea7 (tests)
 end
