e68979f23f19c9cc4bf88b5c9cd4c71985b3cacd
janus_tro <trotzer@campus.tu-berlin.de>
2026-06-26 19:12:06 +0200

    feat: SVD Discretization implemented

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,9 @@ using Test
 @testset verbose=true "SpectralClustering.jl" begin
     include("test_datasets.jl")
     include("test_affinity.jl")
+<<<<<<< HEAD
     include("test_self_tuning.jl")
+=======
+    include("test_discretization.jl")
+>>>>>>> beab6e2 (feat: SVD Discretization implemented)
 end
