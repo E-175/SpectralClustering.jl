1507e9e4ce3608d890b35fc61d62a24705f87095
HabNochKeinKonto <57102564+HabNochKeinKonto@users.noreply.github.com>
2026-06-26 02:15:15 +0200

    Add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,12 @@ using Test
     include("test_datasets.jl")
     include("test_affinity.jl")
 <<<<<<< HEAD
+<<<<<<< HEAD
     include("test_self_tuning.jl")
 =======
     include("test_discretization.jl")
 >>>>>>> beab6e2 (feat: SVD Discretization implemented)
+=======
+    include("test_laplacian.jl")
+>>>>>>> 2e4bfa5 (Add some tests)
 end
